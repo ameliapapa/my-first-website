c31aa5f1738a5da878cda03016ca63568bb7c13d
Amelia Papa <test@MacBook-Pro-76.local>
2024-08-25 11:49:47 +0200

    Initial commit with current version of the project

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var nav = document.getElementById('nav');
+    nav.classList.toggle('show'); // Toggle the 'show' class to show/hide the nav items
+});
